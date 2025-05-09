45c0f6e4c43acf45b3c5a3e0c091d8c6c4931e87
npabhinand <npabinand@gmail.com>
2025-05-09 17:06:15 +0530

    fix: Navigate back to Home Fragment from Transaction Fragment
    This commit changes the navigation behavior of the back button in the Transaction Fragment. Instead of simply popping the back stack, it now specifically navigates back to the Home Fragment using a defined navigation action.

--- app/src/main/java/com/example/dailyexpensetracker/ui/transaction/TransactionFragment.kt
+++ app/src/main/java/com/example/dailyexpensetracker/ui/transaction/TransactionFragment.kt
@@ -129,10 +129,6 @@ class TransactionFragment : Fragment() {
         }
         return type
     }
-    private fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
-        val formatter = SimpleDateFormat(format, locale)
-        return formatter.format(this)
-    }
 
     private fun updateLabel(myCalendar: Calendar) {
         val myFormat = "yyyy/MM/dd"
